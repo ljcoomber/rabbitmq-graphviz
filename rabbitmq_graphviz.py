b68e69dfc69315cd887629d71db174c907a5b2ae
Lee Coomber <lee@lshift.net>
2012-11-03 20:02:58 +0000

    Add support for showing producers

--- rabbitmq_graphviz.py
+++ rabbitmq_graphviz.py
@@ -5,7 +5,7 @@ import sys
 def escape_id(id_str):
     return id_str.replace('-', '').replace('.', '_')
 
-def render_definitions(write, definitions):
+def render_definitions(write, definitions, render_producers):
     write('digraph {')
     write('  bgcolor=transparent;')
     write('  truecolor=true;')
@@ -13,7 +13,7 @@ def render_definitions(write, definitions):
     write('  node [style="filled"];\n')
 
     [render_queue(write, q) for q in definitions['queues']]
-    [render_exchange(write, x) for x in definitions['exchanges']]
+    [render_exchange(write, x, render_producers) for x in definitions['exchanges']]
     [render_binding(write, b) for b in definitions['bindings']]
 
     write('}')
@@ -25,12 +25,14 @@ def render_queue(write, queue):
     write('    "Q_%s" [label="{||||}", fillcolor="red", shape="record"];' % escape_id(queue['name']))
     write('  }\n')
 
-def render_exchange(write, exchange):
+def render_exchange(write, exchange, render_producers):
     write('  subgraph cluster_X_%s {' % escape_id(exchange['name']))
     write('    label="%s\\ntype=%s";' % (exchange['name'], exchange['type']))
     write('    color=transparent;')
     write('    "X_%s" [label="X", fillcolor="#3333CC", shape="ellipse"];' % escape_id(exchange['name']))
     write('  }\n')
+    write('  "P_%s" [label="P", style="filled", fillcolor="#00ffff"];' % escape_id(exchange['name']))
+    write('  "P_%s" -> "X_%s";' % (escape_id(exchange['name']), escape_id(exchange['name'])))
 
 def render_binding(write, binding):
     write('  X_%s -> Q_%s [label="%s"];' % (escape_id(binding['source']), escape_id(binding['destination']), binding['routing_key']))
@@ -41,6 +43,7 @@ def parse_args():
                         help='Definitions file')    
     parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout,
                         help='Output file')
+    parser.add_argument('-p', '--producers', action='store_true', help='Render producers')
     return parser.parse_args()    
 
 if __name__ == '__main__':
@@ -53,5 +56,5 @@ if __name__ == '__main__':
         args.outfile.write(s)
         args.outfile.write('\n')
         
-    render_definitions(writer, definitions)
+    render_definitions(writer, definitions, args.producers)
     args.outfile.close()
